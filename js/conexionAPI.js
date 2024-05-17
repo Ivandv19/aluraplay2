8d7521308453fdc444c78179f9fbffaa443ca977
Ivanmx19 <ivangtxtrab10@gmail.com>
2024-05-17 12:36:25 -0600

    v1

---
+++ js/conexionAPI.js
@@ -0,0 +1,41 @@
+async function listaVideos() {
+    const conexion = await fetch('http://localhost:3001/videos');
+
+    const conexionConvertida = conexion.json();
+
+    return conexionConvertida;
+
+}
+
+
+async function enviarVideo(titulo, descripcion, url, imagen) {
+    const conexion = await fetch('http://localhost:3001/videos', {
+        method: 'POST',
+        headers: { 'Content-type': 'application/json' },
+        body: JSON.stringify({
+            titulo: titulo,
+            descripcion: `${descripcion} mil visualizaciones`,
+            url: url,
+            imagen: imagen
+        })
+    });
+    const conexionConvertida = conexion.json();
+
+    if(!conexion.ok){
+        throw new Error('ha ocurrido un error al enviar el video');
+    }
+
+    return conexionConvertida;
+}
+
+async function buscarVideos(palabraClave) {
+    const conexion = await fetch(`http://localhost:3001/videos?q=${palabraClave}`);
+    const conexionConvertida = conexion.json();
+    return conexionConvertida;
+
+}
+
+export const conexionAPI = {
+    listaVideos, enviarVideo, buscarVideos
+}
+
